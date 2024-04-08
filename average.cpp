1cea8ab215172eb18111d27f891f2ad996244286
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-08 10:49:41 +0530

    static_cast<Data_type>(sorce_variable)

--- average.cpp
+++ average.cpp
@@ -7,11 +7,16 @@ void averageFunction() {
     cout << "Enter Three numbers separted by space" << endl;
     cin >> num1 >> num2 >> num3;
     int count {3};
-    double total = num1 + num2 + num3;
+    // double total = num1 + num2 + num3;
+    int total = num1 + num2 + num3;
     cout << total << endl;
     double average {0.0};
-    average = total/count;   //here, if total is declared int then automatic type conversion happens,
+    // average = total/count; 
+      //here, if total is declared int then automatic type conversion happens,
     //average also convered to int, to avoid this, i declared total as double.
+    average = static_cast<double>(total)/count;   
+    //Line number 17, here total is integer so to convert the total to double data type
+    //we using the static_cast<double>(total) 
     cout << average << endl;
 }
 
