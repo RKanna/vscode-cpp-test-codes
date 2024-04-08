09b92ee6469145652531dea1ac5345ae35afe7d2
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-08 10:38:53 +0530

    Average cpp

---
+++ average.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void averageFunction() {
+    int num1 {}, num2 {}, num3 {};
+    cout << "Enter Three numbers separted by space" << endl;
+    cin >> num1 >> num2 >> num3;
+    int count {3};
+    double total = num1 + num2 + num3;
+    cout << total << endl;
+    double average {0.0};
+    average = total/count;   //here, if total is declared int then automatic type conversion happens,
+    //average also convered to int, to avoid this, i declared total as double.
+    cout << average << endl;
+}
+
+int main() {
+
+    averageFunction();
+    return 0;
+
+}
