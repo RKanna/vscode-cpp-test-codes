f03db951dfe66226a44f158419fd8c9f4e9e10c8
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-09 16:56:24 +0530

    cStyle Sting usage Test

---
+++ CstyleStringUsage.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+int main() {
+
+    char firstName[20] {"hello"};
+    char lastName[20] {};
+    char fullName[50] {};
+
+    cout << strlen(firstName);  //strlen() with cstring
+
+    return 0;
+} 
