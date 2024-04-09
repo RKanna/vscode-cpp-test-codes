285943e0a012d9cf27dd604d4b6115c0b3d8de5b
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-09 16:58:58 +0530

    Tried string manipulation

---
+++ StringManip.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Try this
+
+void stringManipGeneralFunction() {
+      // *****
+      //  ***
+      //   *
+
+      vector<string> nums {"*","*","*","*","*"};
+
+      for(int i {0}; i<nums.size(); i++){
+         cout << nums[i];
+      }
+}
+
+int main() {
+    stringManipGeneralFunction();
+    return 0;
+}
