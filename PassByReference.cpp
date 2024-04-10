ca8281b78fa503380b81baa5ea16384e461d9472
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-10 12:43:03 +0530

    call by reference domo pgm

--- PassByReference.cpp
+++ PassByReference.cpp
@@ -2,3 +2,22 @@
 
 //Actual data not change, but its referenced copy will change
 
+#include <iostream>
+using namespace std;
+//function prototype declarion
+void passByRef(int &val);
+
+void passByRef(int &val) {
+     if(val > 100) {
+        val = 50;
+        cout << val << endl;
+     }
+}
+
+int main() {
+    int num {1000};
+    cout << "Before Function calling value is : " << num << endl;
+    passByRef(num);
+    cout << "After Function calling value is : " << num << endl;
+    return 0;
+}
