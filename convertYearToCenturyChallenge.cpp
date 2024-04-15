68cddc91a5310cb86779cc1937246e0d91cb9a52
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-15 19:13:32 +0530

    WIP, Displaying Century of Year : WIP, Logic need to be completed:

---
+++ convertYearToCenturyChallenge.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+void convertYearToCentury() {
+    int year;
+    int sampleYear = 2000;
+    const int val = 99;
+
+    if(sampleYear){
+        cout << "Its a 20th Century" << endl;
+    }
+}
+
+int main() {
+    return 0;
+
+}
