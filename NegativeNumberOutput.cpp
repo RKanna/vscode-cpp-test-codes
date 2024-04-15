cf3d93b977ec1e13c02a078319ddeef75f47743d
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-15 15:25:24 +0530

    Converting input Integer to Negative Number using C++

--- NegativeNumberOutput.cpp
+++ NegativeNumberOutput.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void negativeInteger(int ){
+void negativeInteger(){
+    int x, result;
+    cout << "Please Enter the Positive Integer" << endl;
+    cin >> x;
+    if(x < 0) {
+     cout << "The Entered Number is negative itself" << endl;
+    }else if(x>0) {
+     int val = -x;
+       cout << "The Negative value is : " << val << endl;
+    }
 
-    
 }
 
 int main() {
-     
-
+     negativeInteger();
      return 0;
 }
