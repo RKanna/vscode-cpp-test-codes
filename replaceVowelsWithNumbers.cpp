b7895daaaa9aa101353f2416f7b52e154fe78b93
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-15 19:14:34 +0530

    replace Vowels With Numbers WIP. logic needed.

---
+++ replaceVowelsWithNumbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+void replaceVowelsWithNumbers() {
+
+    string str;
+    cout << "Replace Vowels with Numbers" << endl;
+    cout << "Enter a String : " << endl;
+    getline(cin, str);
+
+    char vowels[5] = {'a','e','i','o','u'};
+    for(auto val:vowels)
+    {
+        
+    }
+}
+
+int main() {
+    return 0;
+}
