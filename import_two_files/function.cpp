60838fa72d79c0277c6523336af763cedb609e80
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-15 12:06:29 +0530

    Separated C++ files, for modular or segregating different files.

---
+++ import_two_files/function.cpp
@@ -0,0 +1,6 @@
+#include "header.h"
+using namespace std;
+
+int addFunction(int a, int b) {
+    return a+b;
+}
---
+++ import_two_files/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "header.h"
+using namespace std;
+
+int main() {
+   int result = addFunction(5,6);
+    cout << "The Result is : " << result << endl;
+    return 0;
+}
