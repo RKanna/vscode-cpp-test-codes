0f871f33017527933477284fd3956e71d46721cc
rkanna <rkannan.balakrishnan@hotmail.com>
2024-04-22 18:34:29 +0530

    Completed upto Login with number of Login attempts; if user tried 3 or more times with wrong password then it will break from the loop and it will exit.

--- TicketReservationSystem.cpp
+++ TicketReservationSystem.cpp
@@ -7,7 +7,6 @@
 #include <conio.h>
 using namespace std;
 
-
 class ReservationSystem {
 
     struct Data {
@@ -16,17 +15,31 @@ class ReservationSystem {
         string password;
 
         public:
-        //Getpassword and setpassword function for getting and setting password because password declared under the private:
-        
-
+        //setpassword function for setting password because password declared under the private:
         void setPassword(const string& newPassword){
             password = newPassword;
             
         }
+
+        string getPassword() const {
+        return password;
+        }
     };
 
     vector<Data> userVector;
+
+
    public:
+
+   bool doesUserNameExitOrNot(const string& userName){
+
+    for(auto val : userVector){
+        if(val.userName == userName){
+            return true;
+        }
+    }
+    return false;
+   }
      
     void userRegistration() {
       cout << "Hello Its a Ticket Reservation System" << endl;
@@ -57,10 +70,54 @@ class ReservationSystem {
       if(password == confirmPassword)
       {
          userVector.push_back(newData);
-         cout << "User Created Successfully" << endl;
+         cout << "User " << newData.userName << " Created Successfully" << endl;
+         Login();
       }else {
          cout << "Password Mismatch Please Try again" << endl;
       }
+    }
+
+    //After User Successfull Registration
+    void Login(int attempt = 0) {
+
+        if(attempt >= 3){
+             cout << "You tried many Times Please Try again Later" << endl;
+             return;
+        }
+        Data LoginInstance;
+        cout << "Please Enter The User Name : ";
+        cin >> LoginInstance.userName;
+        cout << "Please Enter The Password : ";
+        string LoginPassword;
+        char ch;
+        ch = _getch();
+    while (ch != 13) {
+        LoginPassword.push_back(ch);
+        cout << "*";
+        ch = _getch();
+    }
+    cout << endl;
+        LoginInstance.setPassword(LoginPassword);
+        bool userExist = false;
+        for(auto val:userVector){
+            if(LoginInstance.userName == val.userName){
+                userExist = true;
+                if(LoginPassword == val.getPassword()){
+                     cout << "Login Success";
+                     viewAvailableBus();
+                }else {
+                       cout << "Login Failure" << endl;
+                       Login(attempt +1);
+                       return;
+                }
+            }
+        }
+    }
+
+    void viewAvailableBus(){
+        cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-" << endl;
+        cout << "Welcome To Booking Management System" << endl;
+        cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-" << endl;
     }
 
 };
